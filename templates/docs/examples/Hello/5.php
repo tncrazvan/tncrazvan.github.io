6c7e0895bc75d737565b5dce9588aacf194e4fb2
Razvan <tangent.jotey@gmail.com>
2020-02-18 15:55:31 +0100

    Update 5.php

--- templates/docs/examples/Hello/5.php
+++ templates/docs/examples/Hello/5.php
@@ -3,12 +3,11 @@
 namespace App\Http;
 
 use com\github\tncrazvan\catpaw\tools\Status;
-use com\github\tncrazvan\catpaw\http\HttpEvent;
 use com\github\tncrazvan\catpaw\http\HttpResponse;
 use com\github\tncrazvan\catpaw\http\HttpController;
 
 class Hello extends HttpController{
-    public function &main(HttpEvent &$e, array &$path, string &$content){
+    public function main():HttpResponse{
         return new HttpResponse([
             "Status" => Status::SUCCESS
         ],"hello");
