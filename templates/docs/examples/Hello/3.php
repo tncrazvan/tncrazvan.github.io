354bd904f7fff4bab962e02afbb165552aace817
Razvan <tangent.jotey@gmail.com>
2020-02-18 15:53:11 +0100

    Update 3.php

--- templates/docs/examples/Hello/3.php
+++ templates/docs/examples/Hello/3.php
@@ -6,8 +6,8 @@ use com\github\tncrazvan\catpaw\http\HttpController;
 use com\github\tncrazvan\catpaw\http\HttpEvent;
 
 class Hello extends HttpController{
-    public function &main(HttpEvent &$e, array &$path, string &$content){
-
+    public function main(){
+        return new HttpResponse([],"this is Hello");
     }
     public function onClose():void{}
 }
